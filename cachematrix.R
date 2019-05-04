42fbba94db520e09bdea5943ce2d3d2c89d5bb44
aviaz <aviazharia7@gmail.com>
2019-05-05 02:09:08 +0300

    Complete the programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions that you can see below are used for improve the performance of getting the inverse of matrices
+## by cache the Inverse-Matrix in the memory and get it from the cache when it required, for doing it we need a list of functions that handle
+## cache for each Matrix
 
-## Write a short comment describing this function
-
+## Build the list of function that handle the Cache for Matrix x
 makeCacheMatrix <- function(x = matrix()) {
-
+  inMat <- NULL
+  
+  setMatrix <- function(y) {
+    x <<- y
+    inMat <<- NULL
+  }
+  getMatrix <- function() x
+  setInverseMatrix <- function(y) inMat <<- y
+  getInverseMatrix <- function() inMat
+  
+  list(setMatrix = setMatrix,
+       getMatrix = getMatrix,
+       setInverseMatrix = setInverseMatrix,
+       getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
-
+## Retrieve an Inverse-Matrix from cache if exists or calculate it and Store the Matrix (from list x) and it's inverse-matrix in the cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverseMatrix()
+  if(!is.null(m)) {
+    message("Getting cached Inverse-Matrix")
+    return(m)
+  }
+  data <- x$getMatrix()
+  m <- solve(data)
+  x$setInverseMatrix(m)
+  m
 }
+
+
+## Test function (unrequired)
+test <- function() {
+  mat <- 1:4
+  dim(mat) <- c(2, 2)
+  message("Source Matrix")
+  print(mat)
+  message("Inversed Matrix")
+  x <- makeCacheMatrix(mat)
+  print(cacheSolve(x))
+  mat_r <- cacheSolve(x)
+  mat_r
+  message("Checking that we get Unit Matrix when multiply the matrices:")
+  print(mat %*% mat_r)
+}
+
+test()
+
